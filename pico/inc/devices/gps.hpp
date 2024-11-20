9a2bf4c7e2af2650d0bfc10a20a6c6d74a71e441
rsth <riku.helastera@outlook.com>
2024-11-20 23:42:41 +0200

    gps class, coordinates struct formulating

--- pico/inc/devices/gps.hpp
+++ pico/inc/devices/gps.hpp
@@ -3,10 +3,18 @@
 #include "PicoUart.hpp"
 #include <memory>
 
+struct Coordinates {
+    double latitude;
+    double longitude;
+};
+
 class GPS {
     public:
         GPS(std::shared_ptr<PicoUart> uart);
 
     private: 
+        double latitude;
+        double longitude;
+        
         std::shared_ptr<PicoUart> uart;
 };
