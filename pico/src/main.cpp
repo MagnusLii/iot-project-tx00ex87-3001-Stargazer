730bfb276bef6e24ffb43ae99772017f7c06af49
kkoskivi <122452861+kkoskivi@users.noreply.github.com>
2024-12-08 17:16:47 +0200

    compass returns its current heading when called with getHeading function

--- pico/src/main.cpp
+++ pico/src/main.cpp
@@ -1,5 +1,6 @@
 #include "PicoUart.hpp"
 #include "gps.hpp"
+#include "compass.hpp"
 #include "pico/stdio.h"
 #include "pico/stdlib.h"
 #include <iostream>
@@ -12,8 +13,16 @@ int main() {
     sleep_ms(5000);
     DEBUG("Booted");
 
+    Compass compass;
+    compass.init();
+
+    float heading;
+
     auto uart = std::make_shared<PicoUart>(0, 0, 1, 9600);
     auto gps = std::make_unique<GPS>(uart);
+
+    heading = compass.getHeading();
+    printf("we are pointing at %.2f degrees", heading);
 
     gps->locate_position(600);
     for (;;) {
