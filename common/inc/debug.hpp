f5fbbaf3763b8dd5d6dace1843230eb94539adf9
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2024-12-09 17:50:38 +0200

    Added debug print compatible with espidf

--- common/inc/debug.hpp
+++ common/inc/debug.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
-#ifndef ENABLE_DEBUG
+#if !defined(ENABLE_DEBUG) && !defined(ENABLE_ESP_DEBUG)
 #define DEBUG(...) ((void)0)
-#else
+#endif
+#ifdef ENABLE_DEBUG
 #include <iostream>
 
 namespace dbg {
@@ -10,6 +11,53 @@ template <typename... Args> void print(const char *file, int line, Args... args)
     std::clog << "(DEBUG) [" << file << ":" << line << "] ";
     ((std::clog << args << " "), ...);
     std::clog << std::endl;
+}
+} // namespace dbg
+
+#define DEBUG(...) dbg::print(__FILE__, __LINE__, __VA_ARGS__)
+#endif
+#ifdef ENABLE_ESP_DEBUG
+#include "esp_log.h"
+
+namespace dbg {
+template <typename... Args>
+void print(const char *file, int line, Args... args) {
+    char buffer[256];
+    int offset = snprintf(buffer, sizeof(buffer), "[%s:%d] ", file, line);
+    if (offset < 0 || offset >= sizeof(buffer)) return; // sprintf errors
+
+    auto append_to_buffer = [&buffer, &offset](auto arg) {
+        if (offset < sizeof(buffer)) {
+            if constexpr (std::is_integral_v<decltype(arg)>) {
+                // Integers
+                int written = snprintf(buffer + offset, sizeof(buffer) - offset, "%d ", arg);
+                offset += (written > 0) ? written : 0;
+            } else if constexpr (std::is_floating_point_v<decltype(arg)>) {
+                // Floats
+                int written = snprintf(buffer + offset, sizeof(buffer) - offset, "%.2f ", arg);
+                offset += (written > 0) ? written : 0;
+            } else if constexpr (std::is_same_v<decltype(arg), const char*> || std::is_same_v<decltype(arg), char*>) {
+                // Strings
+                int written = snprintf(buffer + offset, sizeof(buffer) - offset, "%s ", arg);
+                offset += (written > 0) ? written : 0;
+            } else {
+                // Pointers
+                int written = snprintf(buffer + offset, sizeof(buffer) - offset, "<unsupported> ");
+                offset += (written > 0) ? written : 0;
+            }
+        }
+    };
+
+    (append_to_buffer(args), ...);
+
+    // Null terminate.
+    if (offset < sizeof(buffer)) {
+        buffer[offset] = '\0';
+    } else {
+        buffer[sizeof(buffer) - 1] = '\0';
+    }
+
+    ESP_LOGI("(DEBUG)", "%s", buffer);
 }
 } // namespace dbg
 
