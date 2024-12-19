d9d82c1b73ef36d020c2a693e66a89070337dc6e
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2024-12-19 04:14:17 +0200

    Refactor RequestHandler to improve request handling and add error codes

--- esp32-cam/inc/requestHandler.hpp
+++ esp32-cam/inc/requestHandler.hpp
@@ -1,12 +1,11 @@
 #ifndef REQUEST_HANDLER_HPP
 #define REQUEST_HANDLER_HPP
 
+#include "sd-card.hpp"
 #include "wireless.hpp"
 #include <memory>
 #include <string>
-#include "sd-card.hpp"
 
-#define BUFFER_SIZE             1024
 #define ENQUEUE_REQUEST_RETRIES 3
 #define QUEUE_SIZE              10
 
@@ -18,41 +17,50 @@ enum class RequestType {
 };
 
 struct QueueMessage {
-    char request[BUFFER_SIZE];
+    char str_buffer[BUFFER_SIZE];
     char imageFilename[BUFFER_SIZE];
     RequestType requestType;
 };
 
 enum class RequestHandlerReturnCode {
     SUCCESS,
-    UN_CLASSIFIED_ERROR
+    UN_CLASSIFIED_ERROR,
+    DNS_LOOKUP_FAIL,
+    SOCKET_ALLOCATION_FAIL,
+    SOCKET_CONNECT_FAIL,
+    SOCKET_SEND_FAIL,
+    SOCKET_TIMEOUT_FAIL
 };
 
 class RequestHandler {
   public:
-    RequestHandler(std::string webServer, std::string webPort, std::string webPath,
+    RequestHandler(std::string webServer, std::string webPort, std::string webServerToken,
                    std::shared_ptr<WirelessHandler> wirelessHandler, std::shared_ptr<SDcard> sdcard);
 
     RequestHandlerReturnCode createDiagnosticsPOSTRequest(std::string *requestPtr);
     RequestHandlerReturnCode createImagePOSTRequest(std::string *requestPtr);
     RequestHandlerReturnCode createUserInstructionsGETRequest(std::string *requestPtr);
+    QueueMessage* getUserInstructionsGETRequestptr();
 
-    const char *getWebServerCString();
-    const char *getWebPortCString();
+    const char* getWebServerCString();
+    const char* getWebPortCString();
     QueueHandle_t getWebSrvRequestQueue();
     QueueHandle_t getWebSrvResponseQueue();
 
     RequestHandlerReturnCode sendRequest(QueueMessage message);
+    RequestHandlerReturnCode sendRequest(std::string request);
 
   private:
     std::string webServer;
     std::string webPort;
-    std::string webPath;
+    std::string webServerToken;
 
     std::shared_ptr<WirelessHandler> wirelessHandler;
     std::shared_ptr<SDcard> sdcard;
     QueueHandle_t webSrvRequestQueue;
     QueueHandle_t webSrvResponseQueue;
+
+    QueueMessage getUserInsturctionsRequest;
 };
 
 void createTestGETRequest(std::string *request);
