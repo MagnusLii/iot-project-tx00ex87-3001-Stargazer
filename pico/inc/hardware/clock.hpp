b9267e0d75632bdd0145f8b69e3596db735e2274
rikuhel <riku.helastera@metropolia.fi>
2024-12-19 04:00:55 +0200

    pico: add helper method for converting updating time with str

--- pico/inc/hardware/clock.hpp
+++ pico/inc/hardware/clock.hpp
@@ -4,11 +4,13 @@
 #include "pico/stdlib.h"
 #include "pico/util/datetime.h"
 #include "pico/time.h"
+#include <string>
 
 class Clock {
   public:
     Clock();
 
+    void update(std::string &str);
     void update(time_t timestamp);
     datetime_t get_datetime();
     bool is_synced();
