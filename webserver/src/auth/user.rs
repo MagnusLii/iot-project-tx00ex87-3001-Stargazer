94644c5c25d3a10c3afd0cd02b88e2ab9e7e3df1
rikuhel <riku.helastera@metropolia.fi>
2024-12-21 11:15:28 +0200

    webserver: functions to add/remove user, change password

--- webserver/src/auth/user.rs
+++ webserver/src/auth/user.rs
@@ -1,6 +1,6 @@
 use axum_login::AuthUser;
 use serde::{Deserialize, Serialize};
-use sqlx::FromRow;
+use sqlx::{FromRow, SqlitePool};
 
 #[derive(Debug, Clone, Serialize, Deserialize, FromRow)]
 pub struct User {
@@ -19,4 +19,30 @@ impl AuthUser for User {
     fn session_auth_hash(&self) -> &[u8] {
         self.password.as_bytes()
     }
+}
+
+pub async fn add_user(db: &SqlitePool, user: &User) {
+    sqlx::query("INSERT INTO users (username, password) VALUES (?, ?)")
+        .bind(&user.username)
+        .bind(&user.password)
+        .execute(db)
+        .await
+        .unwrap();
+}
+
+pub async fn remove_user(db: &SqlitePool, id: i64) {
+    sqlx::query("DELETE FROM users WHERE id = ?")
+        .bind(id)
+        .execute(db)
+        .await
+        .unwrap();
+}
+
+pub async fn change_password(db: &SqlitePool, id: i64, password: &str) {
+    sqlx::query("UPDATE users SET password = ? WHERE id = ?")
+        .bind(password)
+        .bind(id)
+        .execute(db)
+        .await
+        .unwrap();
 }
