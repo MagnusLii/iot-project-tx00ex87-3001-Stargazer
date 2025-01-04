dfbe2ae484ce46f5fe84e792b4b033a1fd5bb03c
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2025-01-04 03:12:33 +0200

    Enhance SDcard class with mount point accessors and improved error handling during SD card mounting

--- esp32-cam/inc/sd-card.hpp
+++ esp32-cam/inc/sd-card.hpp
@@ -8,8 +8,13 @@ class SDcard {
   public:
     SDcard(std::string mount_point_arg, int max_open_files = 2, int CMD = 15, int D0 = 2,
            int D1 = 4, int D2 = 12, int D3 = 13);
+    
+    std::string* get_mount_point();
+    const char* get_mount_point_c_str();
+    esp_err_t get_sd_card_status();
 
   private:
+    std::string mount_point;
     esp_err_t sd_card_status;
 };
 
