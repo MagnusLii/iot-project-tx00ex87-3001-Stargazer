218a8001684d4b17be217ecfd84f51d7109c7e9a
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2025-01-14 04:40:46 +0200

    refactor: enhance SD card mounting logic with retry mechanism

--- esp32-cam/inc/sd-card.hpp
+++ esp32-cam/inc/sd-card.hpp
@@ -1,16 +1,17 @@
 #ifndef SD_CARD_HPP
 #define SD_CARD_HPP
 
-#include <string>
 #include "esp_err.h"
+#include <string>
 
 class SDcard {
   public:
-    SDcard(std::string mount_point_arg, int max_open_files = 2, int CMD = 15, int D0 = 2,
+    SDcard(std::string mount_point_arg, int max_open_files = 2, int CMD = 15, int D0 = 2, int D1 = 4, int D2 = 12,
+           int D3 = 13);
+    esp_err_t mount_sd_card(std::string mount_point_arg, int max_open_files = 2, int CMD = 15, int D0 = 2,
            int D1 = 4, int D2 = 12, int D3 = 13);
-    
-    std::string* get_mount_point();
-    const char* get_mount_point_c_str();
+    std::string *get_mount_point();
+    const char *get_mount_point_c_str();
     esp_err_t get_sd_card_status();
 
   private:
--- esp32-cam/src/sd-card.cpp
+++ esp32-cam/src/sd-card.cpp
@@ -7,6 +7,24 @@
 #include <string>
 
 SDcard::SDcard(std::string mount_point_arg, int max_open_files, int CMD, int D0, int D1, int D2, int D3) {
+    for (int i = 0; i < 3; i++) {
+        this->sd_card_status = this->mount_sd_card(mount_point_arg, max_open_files, CMD, D0, D1, D2, D3);
+        if (this->sd_card_status == ESP_OK) {
+            DEBUG("SD card mounted at ", mount_point);
+            return;
+        }
+        DEBUG("SD card mount failed, attempt ", i + 1);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+
+    DEBUG("Failed to mount SD card after retries, error: ", esp_err_to_name(sd_card_status));
+    this->sd_card_status = ESP_FAIL;
+    return;
+}
+
+esp_err_t SDcard::mount_sd_card(std::string mount_point_arg, int max_open_files, int CMD, int D0, int D1, int D2,
+                                int D3) {
+
     esp_vfs_fat_sdmmc_mount_config_t mount_config = {.format_if_mount_failed = false,
                                                      .max_files = max_open_files,
                                                      .allocation_unit_size = 16 * 1024,
@@ -26,21 +44,8 @@ SDcard::SDcard(std::string mount_point_arg, int max_open_files, int CMD, int D0,
     gpio_set_pull_mode((gpio_num_t)D2, GPIO_PULLUP_ONLY);  // D2, needed in 4-line mode only
     gpio_set_pull_mode((gpio_num_t)D3, GPIO_PULLUP_ONLY);  // D3, needed in 4- and 1-line modes
 
-    for (int i = 0; i < 3; i++) {
-        this->sd_card_status = esp_vfs_fat_sdmmc_mount(mount_point, &host, &slot_config, &mount_config, &card);
-        if (this->sd_card_status == ESP_OK) {
-            DEBUG("SD card mounted at ", mount_point);
-            return;
-        }
-        DEBUG("SD card mount failed, attempt ", i + 1);
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
-
-    DEBUG("Failed to mount SD card after retries, error: ", esp_err_to_name(sd_card_status));
-    this->sd_card_status = ESP_FAIL;
-    return;
+    return esp_vfs_fat_sdmmc_mount(mount_point, &host, &slot_config, &mount_config, &card);
 }
-
 std::string *SDcard::get_mount_point() { return &this->mount_point; }
 const char *SDcard::get_mount_point_c_str() { return this->mount_point.c_str(); }
 esp_err_t SDcard::get_sd_card_status() { return this->sd_card_status; }
