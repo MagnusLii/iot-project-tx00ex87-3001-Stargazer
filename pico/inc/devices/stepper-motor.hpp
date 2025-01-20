9d51c7be3218b9eb55ea1043518464f1a3d6e009
Ronald5607 <roni.paloniemi@hotmail.com>
2025-01-20 22:13:32 +0200

    added sequence counter

--- pico/inc/devices/stepper-motor.hpp
+++ pico/inc/devices/stepper-motor.hpp
@@ -11,6 +11,9 @@
 #define RPM_MAX 15.
 #define RPM_MIN 1.8
 
+#define CLOCKWISE true
+#define ANTICLOCKWISE false
+
 
 class StepperMotor {
   public:
@@ -42,6 +45,7 @@ class StepperMotor {
     float calculateClkDiv(float rpm) const;
     void morph_pio_pin_definitions(void);
     void pins_init();
+    int read_steps_left(void);
 
     std::vector<uint> pins;          // Stepper motor pins
     std::vector<uint> optoForkPins;          // Pin for opto fork sensor, for now unused
@@ -50,11 +54,11 @@ class StepperMotor {
     uint programOffset;        // Program offset in PIO memory
     uint stateMachine;         // State machine index
     float speed;               // Current motor speed in RPM
-    uint sequenceCounter;      // Sequence counter for step calculation
-    uint stepCounter;          // Total steps taken
+    int8_t sequenceCounter;      // Sequence counter for step calculation
+    int stepCounter;          // Total steps taken
     uint stepMax;              // Maximum number of steps for a full revolution
     uint edgeSteps;            // Steps at the edge of calibration
-    uint stepMemory;           // Tracks recent step movements
+    uint64_t stepMemory;       // Tracks recent step movements
     bool stepperCalibrated;    // Whether the stepper is calibrated
     bool stepperCalibrating;   // Whether calibration is in progress
 };
