363c34d673d7b055ee35d063bcf4843618b3ba16
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2025-02-01 23:02:03 +0200

    feat: add read_file and read_file_base64 methods to SDcardHandler

--- esp32-cam/inc/sd-card.hpp
+++ esp32-cam/inc/sd-card.hpp
@@ -2,37 +2,37 @@
 #define SD_CARD_HPP
 
 #include "esp_err.h"
-#include <string>
-#include <vector>
 #include "freertos/FreeRTOS.h"
 #include "freertos/semphr.h"
+#include <string>
+#include <vector>
 
-enum class Settings{
-  WIFI_SSID,
-  WIFI_PASSWORD,
-  WEB_PORT,
-  WEB_TOKEN,
-  CRC // Keep as final element
+enum class Settings {
+    WIFI_SSID,
+    WIFI_PASSWORD,
+    WEB_PORT,
+    WEB_TOKEN,
+    CRC // Keep as final element
 };
 
 class SDcardHandler {
   public:
-    SDcardHandler(std::string mount_point_arg, int max_open_files = 2, int CMD = 15, int D0 = 2, int D1 = 4, int D2 = 12,
-           int D3 = 13);
+    SDcardHandler(std::string mount_point_arg, int max_open_files = 2, int CMD = 15, int D0 = 2, int D1 = 4,
+                  int D2 = 12, int D3 = 13);
 
     ~SDcardHandler();
-    esp_err_t mount_sd_card(std::string mount_point_arg, int max_open_files = 2, int CMD = 15, int D0 = 2,
-           int D1 = 4, int D2 = 12, int D3 = 13);
-    
+    esp_err_t mount_sd_card(std::string mount_point_arg, int max_open_files = 2, int CMD = 15, int D0 = 2, int D1 = 4,
+                            int D2 = 12, int D3 = 13);
+
     std::string *get_mount_point();
     const char *get_mount_point_c_str();
     esp_err_t get_sd_card_status();
 
-    int write_file(const char* filename, std::string data);
-    int write_file(const char* filename, const char* data);
-    int write_file(const char* filename, const uint8_t* data, const size_t len);
-    int read_file(const char* filename, std::string& data);
-    int read_file(const char* filename, std::vector<uint8_t>& data);
+    int write_file(const char *filename, std::string data);
+    int write_file(const char *filename, const char *data);
+    int write_file(const char *filename, const uint8_t *data, const size_t len);
+    int read_file(const char *filename, std::string &read_data_storage);
+    int read_file_base64(const char *filename, std::string &read_data_storage);
     void add_crc(std::string &data);
     bool check_crc(const std::string &data);
     int save_all_settings(const std::vector<std::string> settings);
