d13919148939f969cd21bacd02c1629295917474
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2025-02-01 23:03:43 +0200

    feat: Create post image pipeline

--- esp32-cam/inc/tasks.hpp
+++ esp32-cam/inc/tasks.hpp
@@ -6,6 +6,9 @@
 
 void get_request_timer_task(void *pvParameters);
 void get_request_timer_task(void *pvParameters);
+bool enqueue_with_retry(const QueueHandle_t queue, const void *item, TickType_t ticks_to_wait, int retries);
+bool read_file_with_retry(SDcardHandler *sdcardHandler, const std::string &filename, std::string &file_data, int retries);
+bool read_file_with_retry_base64(SDcardHandler *sdcardHandler, const std::string &filename, std::string &file_data, int retries);
 
 void init_task(void *pvParameters);
 void send_request_to_websrv_task(void *pvParameters);
