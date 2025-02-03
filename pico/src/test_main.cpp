c05e8bede54963c7f42f9fba0d16714e4d9f4fb0
ronipp <roni.paloniemi@hotmail.com>
2025-02-03 11:57:27 +0200

    testing

--- pico/src/test_main.cpp
+++ pico/src/test_main.cpp
@@ -15,7 +15,28 @@
 #include <sstream>
 #include <vector>
 
+#include "stepper-motor.hpp"
+
 #include "debug.hpp"
+
+// int main() {
+//     stdio_init_all();
+//     Celestial moon(MOON);
+//     datetime_t date;
+//     date.year = 2025;
+//     date.month = 2;
+//     date.day = 2;
+//     date.hour = 20;
+//     date.min = 0;
+//     Coordinates coords(60.1699, 24.9384);
+//     moon.set_observer_coordinates(coords);
+//     datetime_t date2 = moon.get_interest_point_time(ZENITH, date);
+//     moon.print_coordinates(date, 48);
+//     while (true) {
+//         std::cout << (int)date2.month << ", " << (int)date2.day << ", " << (int)date2.hour << std::endl;
+//         sleep_ms(500);
+//     }
+// }
 
 int main() {
     stdio_init_all();
@@ -124,3 +145,4 @@ int main() {
 
     return 0;
 }
+
