f1d2789c979655b55b98c99699d5e3653985f8b1
rikuhel <riku.helastera@metropolia.fi>
2025-02-05 21:49:13 +0200

    webserver: added tls, dual protocol functionality, still WIP

--- webserver/src/app.rs
+++ webserver/src/app.rs
@@ -20,9 +20,10 @@ use axum_login::{
     AuthManagerLayerBuilder,
 };
 use axum_messages::MessagesManagerLayer;
+use axum_server::tls_rustls::RustlsConfig;
 use sqlx::SqlitePool;
+use std::net::TcpListener;
 use time::Duration;
-use tokio::net::TcpListener;
 use tower_http::services::ServeDir;
 
 pub struct App {
@@ -54,7 +55,7 @@ impl App {
         })
     }
 
-    pub async fn serve(self, address: &str) -> Result<(), Box<dyn std::error::Error>> {
+    pub async fn serve(self, address: &str, https: bool) -> Result<(), Box<dyn std::error::Error>> {
         let session_layer = SessionManagerLayer::new(self.session_store)
             .with_secure(false)
             .with_expiry(Expiry::OnInactivity(Duration::days(1)))
@@ -84,28 +85,82 @@ impl App {
             .route("/login", get(login_page))
             .route("/login", post(login))
             .route("/logout", get(logout))
-            .route("/test", get(crate::web::routes::test))
-            .route("/test", post(commands::request_commands_info))
+            //.route("/test", get(crate::web::routes::test))
+            //.route("/test", post(commands::request_commands_info))
             .layer(MessagesManagerLayer)
             .layer(auth_layer)
+            .nest_service("/assets", ServeDir::new("assets"))
             .route(
                 "/api/upload",
                 post(api::upload::upload_image).layer(DefaultBodyLimit::max(262_144_000)),
-                //post(api::upload::print_request),
             )
             .route("/api/command", get(api::commands::fetch_command))
             .route("/api/command", post(api::commands::respond_command))
             .route("/api/diagnostics", post(api::diagnostics::send_diagnostics))
             .route("/api/time", get(api::time_srv::time))
             .with_state(self.shared_state)
-            .nest_service("/assets", ServeDir::new("assets"))
             .fallback(routes::unknown_route);
 
-        let listener = TcpListener::bind(address).await?;
+        if !https {
+            let listener = TcpListener::bind(address)?;
 
-        println!("Listening on: http://{}", address);
+            println!("Listening on: http://{}", address);
 
-        axum::serve(listener, router.into_make_service()).await?;
+            axum_server::from_tcp(listener)
+                .serve(router.into_make_service())
+                .await?;
+        } else {
+            let tls_config =
+                RustlsConfig::from_pem_file("/certs/cert.pem", "/certs/key.pem").await?;
+
+            let listener = TcpListener::bind(address)?;
+
+            println!("Listening on: https://{}", address);
+
+            axum_server::from_tcp_rustls(listener, tls_config)
+                .serve(router.into_make_service())
+                .await?;
+        }
+
+        Ok(())
+    }
+
+    pub async fn serve_api_only(
+        self,
+        address: &str,
+        https: bool,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let router = Router::<_>::new()
+            .route(
+                "/api/upload",
+                post(api::upload::upload_image).layer(DefaultBodyLimit::max(262_144_000)),
+            )
+            .route("/api/command", get(api::commands::fetch_command))
+            .route("/api/command", post(api::commands::respond_command))
+            .route("/api/diagnostics", post(api::diagnostics::send_diagnostics))
+            .route("/api/time", get(api::time_srv::time))
+            .with_state(self.shared_state);
+
+        if !https {
+            let listener = TcpListener::bind(address)?;
+
+            println!("Listening on: http://{} (API Only)", address);
+
+            axum_server::from_tcp(listener)
+                .serve(router.into_make_service())
+                .await?;
+        } else {
+            let tls_config =
+                RustlsConfig::from_pem_file("/certs/cert.pem", "/certs/key.pem").await?;
+
+            let listener = TcpListener::bind(address)?;
+
+            println!("Listening on: https://{} (API Only)", address);
+
+            axum_server::from_tcp_rustls(listener, tls_config)
+                .serve(router.into_make_service())
+                .await?;
+        }
 
         Ok(())
     }
--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -8,9 +8,21 @@ struct Args {
     /// Server address
     #[arg(short, long)]
     address: Option<String>,
-    /// Server port
+    /// HTTP port
     #[arg(short, long)]
-    port: Option<u16>,
+    port_http: Option<u16>,
+    /// HTTPS port
+    #[arg(short = 's', long)]
+    port_https: Option<u16>,
+    /// HTTP disable
+    #[arg(long)]
+    disable_http: bool,
+    /// HTTP allowed for API (Overrides disable_http)
+    #[arg(long)]
+    enable_http_api: bool,
+    /// HTTPS disable
+    #[arg(long)]
+    disable_https: bool,
     /// Database directory
     #[arg(long)]
     db_dir: Option<String>,
@@ -22,20 +34,99 @@ struct Args {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
+    dbg!(&args);
 
-    let settings = Settings::new(args.address, args.port, args.db_dir, args.assets_dir).unwrap();
+    let settings = Settings::new(
+        args.address,
+        args.port_http,
+        args.port_https,
+        if args.disable_http { Some(true) } else { None },
+        if args.enable_http_api {
+            Some(true)
+        } else {
+            None
+        },
+        if args.disable_https { Some(true) } else { None },
+        args.db_dir,
+        args.assets_dir,
+    )
+    .unwrap_or_else(|e| panic!("Error during settings initialization: {}", e));
     settings.print();
 
-    let address = format!("{}:{}", settings.address, settings.port);
+    let http = !settings.disable_http;
+    let http_api = settings.enable_http_api;
+    let https = !settings.disable_https;
+
+    if !http && !http_api && !https {
+        println!("No protocols enabled. Exiting..");
+        return Ok(());
+    }
+
+    let http_address = format!("{}:{}", &settings.address, settings.port_http);
+    let https_address = format!("{}:{}", &settings.address, settings.port_https);
     let user_db_path = format!("{}/users.db", settings.db_dir);
     let api_db_path = format!("{}/api.db", settings.db_dir);
 
     if let Ok(resources) = setup(&user_db_path, &api_db_path, &settings.assets_dir).await {
+        if http && https {
+            tokio::spawn(alt_server(
+                http_address.clone(),
+                resources.user_db.clone(),
+                resources.api_db.clone(),
+                resources.image_dir.clone(),
+                false,
+            ));
+        } else if !http && http_api {
+            tokio::spawn(alt_server(
+                http_address.clone(),
+                resources.user_db.clone(),
+                resources.api_db.clone(),
+                resources.image_dir.clone(),
+                true,
+            ));
+        }
+        let secure = if https { true } else { false };
         App::new(resources.user_db, resources.api_db, resources.image_dir)
             .await?
-            .serve(&address)
+            .serve(
+                if secure {
+                    &https_address
+                } else {
+                    &http_address
+                },
+                secure,
+            )
             .await
     } else {
         panic!("Error during setup. Exiting..");
+    }
+}
+
+async fn alt_server(
+    address: String,
+    user_db: sqlx::SqlitePool,
+    api_db: sqlx::SqlitePool,
+    image_dir: webserver::web::images::ImageDirectory,
+    api_only: bool,
+) {
+    let alt_server;
+    match App::new(user_db, api_db, image_dir).await {
+        Ok(app) => alt_server = app,
+        Err(e) => {
+            println!("Alternate (HTTP) server exited with error: {}", e);
+            return;
+        }
+    }
+
+    if !api_only {
+        match alt_server.serve(&address, false).await {
+            Ok(_) => println!("Alternate (HTTP) server exited"),
+            Err(e) => println!("Alternate (HTTP) server exited with error: {}", e),
+        }
+    } else {
+        match alt_server.serve_api_only(&address, false).await {
+            Ok(_) => println!("Alternate (HTTP API) server exited"),
+            Err(e) => println!("Alternate (HTTP API) server exited with error: {}", e),
+        }
     }
 }
