d830232840aa04989f9d6827e3a066202c0041f9
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2025-02-11 07:22:00 +0200

    feat: add ACK message sending functionality to UART communication handler

--- esp32-cam/inc/espPicoUartCommHandler.hpp
+++ esp32-cam/inc/espPicoUartCommHandler.hpp
@@ -2,10 +2,10 @@
 #define ESP_PICO_COMM_HANDLER_HPP
 
 #include "driver/uart.h"
-#include <stdint.h>
-#include <string>
 #include "requestHandler.hpp"
 #include <memory>
+#include <stdint.h>
+#include <string>
 
 #define UART_RING_BUFFER_SIZE  512
 #define EVENT_QUEUE_SIZE       5
@@ -20,16 +20,15 @@ class EspPicoCommHandler {
   public:
     // UART_NUM_0 is the only viable option for AI-Thinker ESPCAM, UART_NUM_1 is not available and UART_NUM_2 only has a
     // read pin. https://randomnerdtutorials.com/esp32-cam-ai-thinker-pinout/
-    EspPicoCommHandler(uart_port_t uart_num = UART_NUM_0, uart_config_t uart_config = {
-                                                              .baud_rate = 115200,
-                                                              .data_bits = UART_DATA_8_BITS,
-                                                              .parity = UART_PARITY_DISABLE,
-                                                              .stop_bits = UART_STOP_BITS_1,
-                                                              .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-                                                              .rx_flow_ctrl_thresh = 122,
-                                                              .source_clk = UART_SCLK_DEFAULT,
-                                                              .flags = 0
-                                                          });
+    EspPicoCommHandler(uart_port_t uart_num = UART_NUM_0,
+                       uart_config_t uart_config = {.baud_rate = 115200,
+                                                    .data_bits = UART_DATA_8_BITS,
+                                                    .parity = UART_PARITY_DISABLE,
+                                                    .stop_bits = UART_STOP_BITS_1,
+                                                    .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+                                                    .rx_flow_ctrl_thresh = 122,
+                                                    .source_clk = UART_SCLK_DEFAULT,
+                                                    .flags = 0});
 
     void send_data(const char *data, const size_t len);
     int receive_data(char *buffer, const size_t max_len);
@@ -42,8 +41,10 @@ class EspPicoCommHandler {
     void set_waiting_for_response(bool status);
     bool get_waiting_for_response();
 
-    int send_msg_and_wait_for_response(const char* data, const size_t len);
+    int send_msg_and_wait_for_response(const char *data, const size_t len);
     void check_if_confirmation_msg(const UartReceivedData &receivedData);
+
+    void send_ACK_msg(const bool ack);
 
   private:
     uart_port_t uart_num;
