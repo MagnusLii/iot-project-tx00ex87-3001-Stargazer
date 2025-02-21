9e3b8b9304ecbbdfe81499479b4a47eb3beb4881
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2025-02-22 01:10:33 +0200

    test: Add unit tests for saving and reading settings in SDcardHandler

--- esp32-cam/inc/test-cases/test_sd-card.hpp
+++ esp32-cam/inc/test-cases/test_sd-card.hpp
@@ -34,4 +34,25 @@ TEST_CASE("calculate CRC", "[sdcard-handler]") {
     TEST_ASSERT_TRUE(handler.check_crc(data));
 }
 
+TEST_CASE("save and read settings", "[sdcard-handler]") {
+    SDcardHandler handler("/sdcard");
+    std::unordered_map<Settings, std::string> settings;
+    settings[Settings::WIFI_SSID] = "test_ssid";
+    settings[Settings::WIFI_PASSWORD] = "test_password";
+    settings[Settings::WEB_DOMAIN] = "test_domain";
+    settings[Settings::WEB_PORT] = "test_port";
+    settings[Settings::WEB_TOKEN] = "test_token";
+
+    TEST_ASSERT_EQUAL_INT(0, handler.save_all_settings(settings));
+
+    std::unordered_map<Settings, std::string> read_settings;
+    TEST_ASSERT_EQUAL_INT(0, handler.read_all_settings(read_settings));
+
+    TEST_ASSERT_EQUAL_STRING("test_ssid", read_settings[Settings::WIFI_SSID].c_str());
+    TEST_ASSERT_EQUAL_STRING("test_password", read_settings[Settings::WIFI_PASSWORD].c_str());
+    TEST_ASSERT_EQUAL_STRING("test_domain", read_settings[Settings::WEB_DOMAIN].c_str());
+    TEST_ASSERT_EQUAL_STRING("test_port", read_settings[Settings::WEB_PORT].c_str());
+    TEST_ASSERT_EQUAL_STRING("test_token", read_settings[Settings::WEB_TOKEN].c_str());
+}
+
 #endif // TEST_SD_CARD_HPP
