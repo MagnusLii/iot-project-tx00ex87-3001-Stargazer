a9c1cf7b390106d7d66f487e76be14f86f56b879
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2025-02-22 03:07:18 +0200

    refactor: streamline test configuration and enhance test execution flow

--- esp32-cam/test/main/unity_tests.cpp
+++ esp32-cam/test/main/unity_tests.cpp
@@ -1,45 +1,41 @@
+#include "camera.hpp"
+#include "test_jsonParser.hpp"
+#include "test_sd-card.hpp"
 #include "unity.h"
-#include <stdio.h>
-#include <string.h>
 
 // #define JSON_PARSER_TESTS
-#define SDCARD_TESTS
-// #define FAILING_TESTS
-// #define NON_FAILING_TESTS
+// #define SDCARD_TESTS
 // #define ALL_TESTS
 
-#ifdef JSON_PARSER_TESTS
-#include "test_jsonParser.hpp"
-#endif // JSON_PARSER_TESTS
-
-#ifdef SDCARD_TESTS
-#include "test_sd-card.hpp"
-#endif // SDCARD_TESTS
+#ifdef ALL_TESTS
+#define JSON_PARSER_TESTS
+#define SDCARD_TESTS
+#endif // ALL_TESTS
 
 extern "C" {
 void app_main(void);
+void setUp(void);
+void tearDown(void);
 }
+
+void setUp() {}
+
+void tearDown() {}
+
 void app_main(void) {
 
     UNITY_BEGIN();
 #ifdef JSON_PARSER_TESTS
-    unity_run_tests_by_tag("[json-parser]", false);
+    run_all_json_tests();
 #endif // JSON_PARSER_TESTS
 
 #ifdef SDCARD_TESTS
-    unity_run_tests_by_tag("[sdcard-handler]", false);
+    run_all_sd_card_tests();
 #endif // SDCARD_TESTS
 
-#ifdef FAILING_TESTS
-    unity_run_tests_by_tag("[fail]", false);
-#endif // FAILING_TESTS
+#ifdef CAMERA_TESTS
+    run_all_camera_tests();
+#endif // CAMERA_TESTS
 
-#ifdef NON_FAILING_TESTS
-    unity_run_tests_by_tag("[fail]", true);
-#endif // NON_FAILING_TESTS
-
-#ifdef ALL_TESTS
-    unity_run_all_tests();
-#endif // ALL_TESTS
     UNITY_END();
 }
