2c93a5a88d3fd0ecc7308622c676e67600b2937a
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2025-02-23 14:56:56 +0200

    refactor: add request handler tests to unity test suite

--- esp32-cam/test/main/unity_tests.cpp
+++ esp32-cam/test/main/unity_tests.cpp
@@ -1,15 +1,18 @@
 #include "camera.hpp"
 #include "test_jsonParser.hpp"
 #include "test_sd-card.hpp"
+#include "test_requestHandler.hpp"
 #include "unity.h"
 
 // #define JSON_PARSER_TESTS
 // #define SDCARD_TESTS
+#define REQUESTHANDLER_TESTS
 // #define ALL_TESTS
 
 #ifdef ALL_TESTS
 #define JSON_PARSER_TESTS
 #define SDCARD_TESTS
+#define REQUESTHANDLER_TESTS
 #endif // ALL_TESTS
 
 extern "C" {
@@ -17,11 +20,6 @@ void app_main(void);
 void setUp(void);
 void tearDown(void);
 }
-
-void setUp() {}
-
-void tearDown() {}
-
 void app_main(void) {
 
     UNITY_BEGIN();
@@ -33,9 +31,9 @@ void app_main(void) {
     run_all_sd_card_tests();
 #endif // SDCARD_TESTS
 
-#ifdef CAMERA_TESTS
-    run_all_camera_tests();
-#endif // CAMERA_TESTS
+#ifdef REQUESTHANDLER_TESTS
+    run_all_request_handler_tests();
+#endif // REQUESTHANDLER_TESTS
 
     UNITY_END();
 }
