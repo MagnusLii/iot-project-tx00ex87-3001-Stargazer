eaefbd5034f26a2ae3dbc409b5b441e7546fd0b6
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2025-02-24 01:07:49 +0200

    refactor: rename timer task functions for clarity and remove duplicate declaration

--- esp32-cam/inc/tasks.hpp
+++ esp32-cam/inc/tasks.hpp
@@ -18,8 +18,8 @@ struct Handlers {
     std::shared_ptr<CameraHandler> cameraHandler;
 };
 
-void get_request_timer_task(void *pvParameters);
-void get_request_timer_task(void *pvParameters);
+void get_request_timer_callback(void *pvParameters);
+void get_timestamp_timer_callback(void *pvParameters);
 bool enqueue_with_retry(const QueueHandle_t queue, const void *item, TickType_t ticks_to_wait, int retries);
 bool read_file_with_retry(SDcardHandler *sdcardHandler, const std::string &filename, std::string &file_data, int retries);
 bool read_file_with_retry_base64(SDcardHandler *sdcardHandler, const std::string &filename, std::string &file_data, int retries);
@@ -28,7 +28,5 @@ void init_task(void *pvParameters);
 void send_request_to_websrv_task(void *pvParameters);
 void uart_read_task(void *pvParameters);
 void handle_uart_data_task(void *pvParameters);
-
-void take_picture_and_save_to_sdcard_in_loop_task(void *pvParameters);
 
 #endif
