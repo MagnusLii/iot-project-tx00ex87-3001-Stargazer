66be11d51b0144b8d2d45d9e316967626da87a16
rikuhel <riku.helastera@metropolia.fi>
2025-02-24 11:04:52 +0200

    pico: move testmain to main

--- pico/src/main.cpp
+++ pico/src/main.cpp
@@ -1,40 +1,41 @@
+#include "pico/stdlib.h" // IWYU pragma: keep
+
 #include "PicoUart.hpp"
 #include "clock.hpp"
+#include "commbridge.hpp"
 #include "compass.hpp"
-#include "gps.hpp"
-#include "compass.hpp"
-#include "stepper-motor.hpp"
-#include "compass.hpp"
-#include "pico/stdio.h"
-#include "pico/stdlib.h"
-#include <hardware/rtc.h>
-#include <iostream>
-#include <memory>
-#include <vector>
-
+#include "controller.hpp"
 #include "debug.hpp"
+#include "gps.hpp"
+#include "message.hpp"
+
+#include <memory>
+#include <queue>
 
 int main() {
     stdio_init_all();
-    DEBUG("Booted\n");
+    sleep_ms(500);
+    DEBUG("Boot");
 
-    // uint stepperPins[] = {2, 3, 6, 13};
-    std::vector<uint> stepperPins{2, 3, 6, 13};
-    // std::vector<uint> stepperPins2{18, 19, 20, 21};
-    uint optofork(0);
+    auto uart_0 = std::make_shared<PicoUart>(0, 0, 1, 115200);
+    auto uart_1 = std::make_shared<PicoUart>(1, 4, 5, 9600);
+    sleep_ms(50);
 
-    PIO pio = pio0;
-    // StepperMotor motor2(stepperPins2, optoforks);
-    
-    // motor2.init(pio1, 5, true);
+    auto clock = std::make_shared<Clock>();
+    auto gps = std::make_shared<GPS>(uart_1, false, true);
+    auto compass = std::make_shared<Compass>(17, 16, i2c0);
 
-    // motor2.turnSteps(10000);
-    while (true) {
-        for (auto pin : stepperPins) {
-            std::cout << gpio_get(pin) << ", ";
-        }
-        std::cout << std::endl;
+    auto queue = std::make_shared<std::queue<msg::Message>>();
+    auto commbridge = std::make_shared<CommBridge>(uart_0, queue);
+
+    std::vector<uint> pins_temp{1,2,3,4};
+    auto motor_horizontal = std::make_shared<StepperMotor>(pins_temp, pins_temp);
+    auto motor_vertical = std::make_shared<StepperMotor>(pins_temp, pins_temp);
+    Controller controller(clock, gps, compass, commbridge, motor_vertical, motor_horizontal, queue);
+    for (;;) {
+        controller.run();
+
+        DEBUG("Returned from main loop");
+        sleep_ms(1000);
     }
-
-    return 0;
 }
