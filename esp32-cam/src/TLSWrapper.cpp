02477b4fd3da3b35f9c079c33f2d46443d58b507
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2025-03-04 18:39:39 +0200

    config: enhance TLS connection logging and improve certificate handling

--- esp32-cam/src/TLSWrapper.cpp
+++ esp32-cam/src/TLSWrapper.cpp
@@ -81,17 +81,6 @@ bool TLSWrapper::connect(const char *host, const char *port, const char *root_ce
         return false;
     }
 
-    if (strlen(root_cert) == 0) {
-        DEBUG("Invalid arguments: root_cert is empty");
-        return false;
-    }
-
-    DEBUG("Free heap before TLS connection: %d", esp_get_free_heap_size());
-    DEBUG("Smallest free heap before TLS connection: %d", esp_get_minimum_free_heap_size());
-    multi_heap_info_t info;
-    heap_caps_get_info(&info, MALLOC_CAP_8BIT);
-    DEBUG("Largest free block: %d", info.largest_free_block);
-
     int ret;
     size_t cert_len = strlen(root_cert) + 1;
 
@@ -100,15 +89,14 @@ bool TLSWrapper::connect(const char *host, const char *port, const char *root_ce
         return false;
     }
 
-    // Remove redundant net_ctx initialization
     ret = mbedtls_net_connect(&this->net_ctx, host, port, MBEDTLS_NET_PROTO_TCP);
-    DEBUG("Connection done");
     if (ret != 0) {
         char error_buf[100];
         mbedtls_strerror(ret, error_buf, sizeof(error_buf));
         DEBUG("TCP connection failed, error code: ", ret, " - ", error_buf);
         return false;
     }
+    DEBUG("Connection done");
 
     int conf_ret = mbedtls_ssl_config_defaults(&this->ssl_conf, MBEDTLS_SSL_IS_CLIENT, MBEDTLS_SSL_TRANSPORT_STREAM,
                                                MBEDTLS_SSL_PRESET_DEFAULT);
@@ -116,25 +104,32 @@ bool TLSWrapper::connect(const char *host, const char *port, const char *root_ce
         DEBUG("SSL config failed, error code: ", conf_ret);
         return false;
     }
+    DEBUG("SSL config done");
 
 #ifdef DISABLE_CERTIFICATE_VERIFICATION
     DEBUG("Disabling certificate verification");
     mbedtls_ssl_conf_authmode(&this->ssl_conf, MBEDTLS_SSL_VERIFY_NONE);
 #else
     mbedtls_ssl_conf_authmode(&this->ssl_conf, MBEDTLS_SSL_VERIFY_REQUIRED);
+    DEBUG("Enabling certificate verification");
 
-    mbedtls_x509_crt_free(&this->ca_cert);
+    if (this->ca_cert.next != nullptr) {
+        mbedtls_x509_crt_free(&this->ca_cert);
+    }
     mbedtls_x509_crt_init(&this->ca_cert);
+    DEBUG("Parsing CA certificate");
 
     ret = mbedtls_x509_crt_parse(&this->ca_cert, (const unsigned char *)root_cert, cert_len);
     if (ret != 0) {
         char error_buf[100];
         mbedtls_strerror(ret, error_buf, sizeof(error_buf));
-        DEBUG("Failed to parse CA certificate, error code: ", ret, " - ", error_buf);
+        DEBUG("Failed to parse CA certificate, error code: %d - %s", ret, error_buf);
         return false;
     }
+    DEBUG("CA certificate parsed");
 
     mbedtls_ssl_conf_ca_chain(&this->ssl_conf, &this->ca_cert, NULL);
+    DEBUG("CA certificate chain set");
 #endif
 
     mbedtls_ssl_conf_rng(&this->ssl_conf, mbedtls_ctr_drbg_random, &this->ctr_drbg);
@@ -143,13 +138,14 @@ bool TLSWrapper::connect(const char *host, const char *port, const char *root_ce
         DEBUG("mbedtls_ssl_setup failed, error code: ", ret);
         return false;
     }
+    DEBUG("SSL setup done");
 
     mbedtls_ssl_set_bio(&this->ssl, &this->net_ctx, mbedtls_net_send, mbedtls_net_recv, nullptr);
-
     if (mbedtls_ssl_set_hostname(&this->ssl, "stargazer") != 0) {
         DEBUG("Setting hostname failed");
         return false;
     }
+    DEBUG("Hostname set");
 
     ret = mbedtls_ssl_handshake(&this->ssl);
     if (ret != 0) {
