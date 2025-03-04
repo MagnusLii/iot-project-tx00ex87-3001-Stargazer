8ceccd32339fe9b556f28994efa8613615ab81a8
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2025-03-04 18:37:29 +0200

    config: add debug logging for mutex acquisition and release in ScopedMutex class

--- esp32-cam/inc/scopedMutex.hpp
+++ esp32-cam/inc/scopedMutex.hpp
@@ -3,13 +3,20 @@
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/semphr.h"
+#include "debug.hpp"
 
 // Automatically unlocks the semaphore when the object goes out of scope
 // Takes a semaphore and locks it in the constructor, unlocks it in the destructor
 class ScopedMutex {
   public:
-    ScopedMutex(SemaphoreHandle_t &m) : mutex(m) { xSemaphoreTake(mutex, portMAX_DELAY); }
-    ~ScopedMutex() { xSemaphoreGive(mutex); }
+    ScopedMutex(SemaphoreHandle_t &m) : mutex(m) {
+        DEBUG("Taking mutex");
+        xSemaphoreTake(mutex, portMAX_DELAY);
+    }
+    ~ScopedMutex() {
+        DEBUG("Giving mutex");
+        xSemaphoreGive(mutex);
+    }
 
   private:
     SemaphoreHandle_t &mutex;
