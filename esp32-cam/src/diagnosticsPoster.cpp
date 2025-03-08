d2110d66c525d135dc61d2d857e8c802698c8f8f
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2025-03-08 14:13:28 +0200

    Create diagnosticsposter for easy diagnostics msgs to webserver

--- esp32-cam/src/diagnosticsPoster.cpp
+++ esp32-cam/src/diagnosticsPoster.cpp
@@ -1,6 +1,14 @@
 #include "diagnosticsPoster.hpp"
 
-DiagnosticsPoster::DiagnosticsPoster(QueueHandle_t *webSrvRequestQueue) {
-    this->webSrvRequestQueue = webSrvRequestQueue;
+DiagnosticsPoster::DiagnosticsPoster(std::shared_ptr<RequestHandler> requestHandler,
+                                     std::shared_ptr<WirelessHandler> wirelessHandler) {
+    this->requestHandler = requestHandler;
+    this->wirelessHandler = wirelessHandler;
 }
 
+bool DiagnosticsPoster::add_diagnostics_to_queue(std::string message, DiagnosticsStatus status_level) {
+    std::string request;
+    this->requestHandler->createGenericPOSTRequest(&request, "/api/diagnostics", "token",
+                                                   wirelessHandler->get_setting(Settings::WEB_TOKEN), "status",
+                                                   static_cast<int>(status_level), "message", message.c_str());
+}
