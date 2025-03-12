10a2bece01cfba1d7e7fca027c59691820c37baf
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2025-03-12 14:59:33 +0200

    docs: add detailed documentation for WirelessHandler class methods

--- esp32-cam/src/wireless.cpp
+++ esp32-cam/src/wireless.cpp
@@ -18,6 +18,17 @@
 #define WIFI_CONNECTED_BIT BIT0
 #define WIFI_FAIL_BIT      BIT1
 
+/**
+ * @brief Constructor for the WirelessHandler class.
+ * 
+ * This constructor initializes the WirelessHandler instance, setting up the
+ * SDcardHandler, retry attempts for WiFi connection, and other internal variables.
+ * It also creates a mutex for managing concurrent access to the wireless resources.
+ * 
+ * @param sdcardhandler A shared pointer to an SDcardHandler instance used for handling SD card operations.
+ * 
+ * @return void
+ */
 WirelessHandler::WirelessHandler(std::shared_ptr<SDcardHandler> sdcardhandler) {
     this->sdcardHandler = sdcardhandler;
 
@@ -29,6 +40,18 @@ WirelessHandler::WirelessHandler(std::shared_ptr<SDcardHandler> sdcardhandler) {
     this->wifi_mutex = xSemaphoreCreateMutex();
 }
 
+/**
+ * @brief Initializes the Wi-Fi stack and related resources.
+ * 
+ * This method sets up the necessary components for Wi-Fi functionality, including:
+ * - Initializing Non-Volatile Storage (NVS) for Wi-Fi configuration.
+ * - Creating the Wi-Fi event group and event loop.
+ * - Setting default Wi-Fi handlers and creating a default Wi-Fi station network interface.
+ * - Configuring the Wi-Fi stack and registering event handlers for Wi-Fi and IP events.
+ * - Setting a custom hostname for the device.
+ * 
+ * @return esp_err_t Returns ESP_OK on success or an error code on failure.
+ */
 esp_err_t WirelessHandler::init(void) {
     DEBUG("Initializing Wi-Fi");
     ScopedMutex lock(this->wifi_mutex);
@@ -92,6 +115,24 @@ esp_err_t WirelessHandler::init(void) {
     return ret;
 }
 
+/**
+ * @brief Connects to a Wi-Fi network using the provided SSID and password.
+ * 
+ * This method attempts to connect to the specified Wi-Fi network by:
+ * - Configuring the Wi-Fi credentials (SSID and password).
+ * - Setting the Wi-Fi mode to Station (STA).
+ * - Starting the Wi-Fi connection process.
+ * - Retries the connection a predefined number of times (based on `WIFI_RETRY_ATTEMPTS`), waiting for a connection event.
+ * 
+ * The method will return:
+ * - `ESP_OK` if the connection is successfully established.
+ * - `ESP_FAIL` if the connection fails after the maximum retry attempts.
+ * 
+ * @param wifi_ssid The SSID of the Wi-Fi network to connect to.
+ * @param wifi_password The password for the Wi-Fi network.
+ * 
+ * @return esp_err_t Returns `ESP_OK` if connected successfully, or an error code (`ESP_FAIL`) if the connection fails.
+ */
 esp_err_t WirelessHandler::connect(const char *wifi_ssid, const char *wifi_password) {
     ScopedMutex lock(this->wifi_mutex);
     DEBUG("Connecting to Wi-Fi network: ", wifi_ssid);
@@ -121,6 +162,18 @@ esp_err_t WirelessHandler::connect(const char *wifi_ssid, const char *wifi_passw
     return ESP_FAIL;
 }
 
+/**
+ * @brief Disconnects from the current Wi-Fi network.
+ * 
+ * This method attempts to disconnect from the currently connected Wi-Fi network by:
+ * - Calling `esp_wifi_disconnect()` to initiate the disconnection.
+ * - Deleting the Wi-Fi event group used for connection status.
+ * 
+ * If disconnection is successful, it returns the result of the `esp_wifi_disconnect()` call.
+ * If the disconnection fails, an error message is logged with the corresponding error code.
+ * 
+ * @return esp_err_t Returns `ESP_OK` if disconnection is successful, or an error code if it fails.
+ */
 esp_err_t WirelessHandler::disconnect(void) {
     ScopedMutex lock(this->wifi_mutex);
     DEBUG("Disconnecting from Wi-Fi network");
@@ -136,6 +189,20 @@ esp_err_t WirelessHandler::disconnect(void) {
     return err;
 }
 
+/**
+ * @brief Deinitializes the Wi-Fi subsystem.
+ * 
+ * This method stops the Wi-Fi driver, deinitializes the Wi-Fi subsystem, and clears the default Wi-Fi driver and handlers.
+ * It also destroys the Wi-Fi network interface and unregisters any event handlers related to Wi-Fi and IP events.
+ * 
+ * The function performs the following steps:
+ * - Stops the Wi-Fi driver using `esp_wifi_stop()`.
+ * - Deinitializes the Wi-Fi driver and clears default Wi-Fi handlers.
+ * - Destroys the network interface.
+ * - Unregisters event handlers for IP and Wi-Fi events.
+ * 
+ * @return esp_err_t Returns `ESP_OK` if the Wi-Fi subsystem is deinitialized successfully, or an error code if any operation fails.
+ */
 esp_err_t WirelessHandler::deinit(void) {
     ScopedMutex lock(this->wifi_mutex);
     DEBUG("Deinitializing Wi-Fi");
@@ -158,10 +225,32 @@ esp_err_t WirelessHandler::deinit(void) {
     return ESP_OK;
 }
 
+/**
+ * @brief Checks if the device is connected to a Wi-Fi network.
+ * 
+ * This method checks the status of the Wi-Fi connection by examining the Wi-Fi event group.
+ * It returns `true` if the device is connected to a Wi-Fi network, and `false` otherwise.
+ * 
+ * @return bool Returns `true` if the device is connected to Wi-Fi, otherwise `false`.
+ */
 bool WirelessHandler::isConnected(void) {
     return this->s_wifi_event_group && (xEventGroupGetBits(this->s_wifi_event_group) & WIFI_CONNECTED_BIT);
 }
 
+/**
+ * @brief Callback function to handle IP-related events.
+ * 
+ * This function handles various IP events such as obtaining an IPv4 or IPv6 address,
+ * and losing an IP address. It processes the IP event and updates the Wi-Fi event group 
+ * accordingly. It also logs information about the event, including the IP addresses.
+ * 
+ * @param arg A pointer to the argument passed during the event registration. Typically unused.
+ * @param event_base The event base that triggered this callback.
+ * @param event_id The specific event ID associated with the IP event (e.g., IP_EVENT_STA_GOT_IP, IP_EVENT_STA_LOST_IP, IP_EVENT_GOT_IP6).
+ * @param event_data A pointer to the event-specific data (contains IP information).
+ * 
+ * @note This function updates the Wi-Fi event group to indicate whether the device is connected or lost the connection.
+ */
 void WirelessHandler::ip_event_cb(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
     DEBUG("Handling IP event, event code: ", event_id);
     ip_event_got_ip_t *event_ip = nullptr;
@@ -187,6 +276,20 @@ void WirelessHandler::ip_event_cb(void *arg, esp_event_base_t event_base, int32_
     }
 }
 
+/**
+ * @brief Callback function to handle Wi-Fi related events.
+ * 
+ * This function handles various Wi-Fi events, including Wi-Fi initialization, connection attempts, 
+ * connection status changes, and authentication mode changes. It processes the event and logs the relevant
+ * information, and takes actions such as retrying the connection or attempting to reconnect.
+ * 
+ * @param arg A pointer to the argument passed during the event registration. Typically unused.
+ * @param event_base The event base that triggered this callback.
+ * @param event_id The specific event ID associated with the Wi-Fi event (e.g., WIFI_EVENT_STA_CONNECTED, WIFI_EVENT_STA_DISCONNECTED).
+ * @param event_data A pointer to the event-specific data (contains Wi-Fi event information).
+ * 
+ * @note This function also handles the Wi-Fi reconnection logic in case of disconnections.
+ */
 void WirelessHandler::wifi_event_cb(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
     DEBUG("Handling Wi-Fi event, event code: ", event_id);
 
@@ -222,25 +325,95 @@ void WirelessHandler::wifi_event_cb(void *arg, esp_event_base_t event_base, int3
     }
 }
 
+/**
+ * @brief Retrieves the value of a specific setting.
+ * 
+ * This function returns the value of the specified setting as a constant C-string. The setting is 
+ * identified by the provided `settingID`. The settings are stored internally in a container, and this 
+ * function retrieves the value associated with the given setting ID.
+ * 
+ * @param settingID The ID of the setting to retrieve.
+ * @return A pointer to a constant C-string representing the value of the requested setting.
+ * 
+ * @note The returned string is a reference to an internal value, so its lifetime is tied to the 
+ *       lifespan of the `WirelessHandler` instance.
+ */
 const char *WirelessHandler::get_setting(Settings settingID) { return this->settings[settingID].c_str(); }
 
+/**
+ * @brief Retrieves a pointer to the container holding all settings.
+ * 
+ * This function returns a pointer to the internal `std::unordered_map` that stores all settings.
+ * The map associates `Settings` keys with their corresponding values stored as `std::string`.
+ * 
+ * @return A pointer to the `std::unordered_map` containing all settings.
+ * 
+ * @note Modifying the returned map directly will affect the internal settings of the `WirelessHandler` instance.
+ */
 std::unordered_map<Settings, std::string>* WirelessHandler::get_all_settings_pointer() { return &this->settings; }
 
+/**
+ * @brief Sets a setting value for a specified setting ID.
+ * 
+ * This function assigns the provided buffer content to the setting specified by `settingID`. 
+ * It copies `buffer_len` characters from the given buffer into the corresponding setting value. 
+ * If the buffer is invalid (null or empty), the operation is aborted and returns an error.
+ * 
+ * @param buffer A pointer to the buffer containing the setting value to be set.
+ * @param buffer_len The length of the buffer (number of characters) to be copied.
+ * @param settingID The ID of the setting to be updated.
+ * 
+ * @return `0` on success, or `-1` if the buffer is invalid (null or empty).
+ */
 int WirelessHandler::set_setting(const char *buffer, const size_t buffer_len, Settings settingID) {
     if (buffer == nullptr || buffer_len == 0) return -1; // Prevent invalid writes
     this->settings[settingID] = std::string(buffer, buffer_len);
     return 0;
 }
 
+/**
+ * @brief Sets multiple settings at once by updating the internal settings map.
+ * 
+ * This function updates all settings by replacing the current settings map with the 
+ * provided one. The `settings` map must contain key-value pairs where the keys are 
+ * `Settings` IDs and the values are the corresponding setting values as strings.
+ * 
+ * @param settings A map of settings to be set, where keys are setting IDs and values are setting values.
+ * 
+ * @return `true` if the settings were successfully updated.
+ */
 bool WirelessHandler::set_all_settings(std::unordered_map<Settings, std::string> settings) {
     this->settings = settings;
     return true;
 }
 
+/**
+ * @brief Saves the provided settings to the SD card.
+ * 
+ * This function delegates the task of saving the settings to the `sdcardHandler`'s 
+ * `save_all_settings` function. The provided `settings` map will be written to the SD card.
+ * 
+ * @param settings A map of settings to be saved, where keys are setting IDs and values are setting values.
+ * 
+ * @return The result of the `save_all_settings` function from the `sdcardHandler`. 
+ *         A non-negative value typically indicates success, while a negative value indicates failure.
+ */
 int WirelessHandler::save_settings_to_sdcard(std::unordered_map<Settings, std::string> settings) {
     return this->sdcardHandler->save_all_settings(settings);
 }
 
+/**
+ * @brief Reads settings from the SD card and stores them in the provided map.
+ * 
+ * This function delegates the task of reading settings from the SD card to the `sdcardHandler`'s 
+ * `read_all_settings` function. The provided `settings` map will be populated with the settings 
+ * read from the SD card.
+ * 
+ * @param settings A map to store the read settings, where keys are setting IDs and values are setting values.
+ * 
+ * @return The result of the `read_all_settings` function from the `sdcardHandler`. 
+ *         A non-negative value typically indicates success, while a negative value indicates failure.
+ */
 int WirelessHandler::read_settings_from_sdcard(std::unordered_map<Settings, std::string> settings) {
     return this->sdcardHandler->read_all_settings(settings);
 }
